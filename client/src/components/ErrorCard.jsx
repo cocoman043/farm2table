e9438a3393c8a1dd245b41cc7dddd0b337ab8b25
cocoman043 <rocampita@up.edu.ph>
2024-05-15 17:23:38 +0800

    chore: removed double width style
    w-96 and w-fit do the same thing

--- client/src/components/ErrorCard.jsx
+++ client/src/components/ErrorCard.jsx
@@ -4,7 +4,7 @@ import { faWarning } from "@fortawesome/free-solid-svg-icons";
 function ErrorCard() {
   return (
     <>
-      <div className="card bg-indigo-950 w-96 shadow-xl w-fit">
+      <div className="card bg-indigo-950 shadow-xl w-fit">
         <div className="card-body">
           <h2 className="card-title gap-8"><FontAwesomeIcon icon={faWarning} size="2xl" />404 Not Found</h2>
           <p>Please report this bug to the developers!</p>
