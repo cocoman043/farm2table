f60f8891f17492eb59ed527ed9b016904f3f6ebd
Yan1an <cgberana@up.edu.ph>
2024-05-18 13:55:32 +0800

    feat: Setup product model and controller

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,1 +1,13 @@
-// TODO: Create Product Schema
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  desciption: { type: String, required: true },
+  type: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true }
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+export default Product;
