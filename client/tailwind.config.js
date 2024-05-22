bb50270a63c2aab50fb335bae5aec68d750f7528
cocoman043 <rocampita@up.edu.ph>
2024-05-22 10:15:33 +0800

    fix: updated order contoller

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-pattern': "url('./public/black.svg')",
+      }
+    },
   },
   plugins: [
     daisyui
