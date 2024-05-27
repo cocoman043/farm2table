4fdd187cf8e20f73f0f0455c240a9d8b48629687
Jules <jgbanaag1@up.edu.ph>
2024-05-27 20:29:18 +0800

    feat: added delete and edit button components

---
+++ client/src/components/EditButton.jsx
@@ -0,0 +1,10 @@
+function EditButton({ onClick }) {
+    
+    return (
+        <button className="text-black btn btn-warning" onClick={onClick}>
+        Edit
+        </button>
+    );
+}
+
+export default EditButton;
