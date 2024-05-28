7fa32ce2fbe2a583ed166cf8a77080a14f5db119
Jules <jgbanaag1@up.edu.ph>
2024-05-29 00:11:53 +0800

    fix: updated auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,41 +1,59 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import 'dotenv/config.js';
 
 const register = async (req, res) => {
-    const { fullName, email, password } = req.body;
-    const user = new User({ fullName, email, password });
+    const { username, email, password } = req.body;
+
     try {
+        // Check if the email is already in use
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email is already in use." });
+        }
+
+        const user = new User({ username, email, password });
+
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const user = new User({ fullName, email, password: hashedPassword });
+        user.password = hashedPassword;
         await user.save();
 
-        // generate JWT token
-        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+        // Generate JWT token
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.status(201).send("User registered with token ", token);
+        res.status(201).json({ message: "User registered", token });
     } catch (error) {
-        res.status(400).send("Error registering user.");
+        console.error('Error registering user:', error);
+        res.status(400).json({ message: "Error registering user.", error: error.message });
     }
 };
 
 const login = async (req, res) => {
     const { email, password } = req.body;
+
     try {
         const user = await User.findOne({ email });
-        if (!user) return res.status(400).send("User not found.");
+        if (!user) {
+            return res.status(400).json({ message: "Email not found." });
+        }
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(400).send("Invalid password.");
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid password." });
+        }
 
         // generate JWT token
-        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const id = user.id;
+        const userType = user.userType;
 
-        res.status(200).json({ token });
+        res.status(200).json({ id, userType, token });
     } catch (error) {
-        res.status(500).send("Server error.");
+        console.error('Error during login:', error);
+        res.status(500).json({ message: "Server error.", error: error.message });
     }
 };
 
