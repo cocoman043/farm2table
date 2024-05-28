d5e8b37cb7af5d9ea342237f1f9a5cc904bc4d2c
Jules <jgbanaag1@up.edu.ph>
2024-05-28 14:34:48 +0800

    feat: added encryption to password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,21 @@
 import User from "../models/User.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const register = async (req, res) => {
     const { fullName, email, password } = req.body;
     const user = new User({ fullName, email, password });
     try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({ fullName, email, password: hashedPassword });
         await user.save();
-        res.status(201).send("User registered.");
+
+        // generate JWT token
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(201).send("User registered with token ", token);
     } catch (error) {
         res.status(400).send("Error registering user.");
     }
@@ -17,9 +27,13 @@ const login = async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(400).send("User not found.");
 
-        if (user.password !== password) return res.status(400).send("Invalid password.");
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).send("Invalid password.");
 
-        res.status(200).send("Login successful.");
+        // generate JWT token
+        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({ token });
     } catch (error) {
         res.status(500).send("Server error.");
     }
